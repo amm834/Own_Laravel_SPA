49fdb8b00784d37183d9b8fa50182b02590e91da
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-15 18:40:12 +0630

    Created API CRUD

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Requests\ProductCreateRequest;
+use App\Http\Requests\ProductUpdateRequest;
+class ProductController extends Controller
+{
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index() {
+    return Product::orderByDesc('id')->get();
+  }
+
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(ProductCreateRequest $request) {
+    return Product::create($request->only('name', 'price'));
+  }
+
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show(Product $product) {
+    return $product;
+  }
+
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(ProductUpdateRequest $request, Product $product) {
+    return $product->update($request->only('name', 'price'));
+  }
+
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy(Product $product) {
+    return $product->delete();
+  }
+}
